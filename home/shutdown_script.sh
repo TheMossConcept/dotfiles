07cba53ea8dbb0675a673eb36d44b61e79e33b17
Niklas Moss <niklas@themossconcept.com>
2026-01-30 10:05:22 +0000

    Properly update dotfiles on startup and do not change account when pushing them on shutdown

--- home/shutdown_script.sh
+++ home/shutdown_script.sh
@@ -3,11 +3,8 @@ pass git push
 
 # Update dotfiles
 cd ~/.homesick/repos/dotfiles
-# We assume that we are always authorised as TheMossConcept
-gh auth switch --user Nnoerregaard
 git add -A
 git commit -m "Dotfiles updated on shutdown"
 git push
-gh auth switch --user TheMossConcept
 
 shutdown
