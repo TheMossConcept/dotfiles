11b458f235d54735f2a32ca141b8ef872f7daef1
Niklas Moss <niklas@themossconcept.com>
2026-03-10 11:02:04 +0000

    Do not mess with neovim panes on tmux resizing

--- home/.config/nvim/lua/init.lua
+++ home/.config/nvim/lua/init.lua
@@ -1,5 +1,8 @@
 require("config.lazy")
 
+-- To avoid vim resizing panes on maximising/minimising in tmux
+vim.o.equalalways = false
+
 -- Workaround for diagnostics disappearing in insert mode causing the whole UI to continously jump around 
 vim.diagnostic.config({
     update_in_insert = true,
